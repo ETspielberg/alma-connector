6eba3c543463b2c39d9d5eb6aff09c320ced27bf
Eike Spielberg <Eike.Spielberg@uni-due.de>
2021-01-14 14:37:36 +0100

    retrieving items from alma

---
+++ src/main/java/org/unidue/ub/libintel/almaconnector/service/ItemService.java
@@ -0,0 +1,20 @@
+package org.unidue.ub.libintel.almaconnector.service;
+
+import org.springframework.stereotype.Service;
+import org.unidue.ub.alma.shared.bibs.Item;
+import org.unidue.ub.libintel.almaconnector.clients.acquisition.AlmaItemsApiClient;
+
+@Service
+public class ItemService {
+
+    private final AlmaItemsApiClient almaItemsApiClient;
+
+    public ItemService(AlmaItemsApiClient almaItemsApiClient) {
+        this.almaItemsApiClient = almaItemsApiClient;
+    }
+
+    public Item findItemByBarcode(String barcode) {
+        return this.almaItemsApiClient.getItem("application/json", barcode, "");
+
+    }
+}
