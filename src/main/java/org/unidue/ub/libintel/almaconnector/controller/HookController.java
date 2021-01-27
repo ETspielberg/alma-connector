afabef92cbb9ad45194847ac31236b6b724a0ec8
Eike Spielberg <Eike.Spielberg@uni-due.de>
2021-01-27 15:21:54 +0100

    logging hook content

--- src/main/java/org/unidue/ub/libintel/almaconnector/controller/HookController.java
+++ src/main/java/org/unidue/ub/libintel/almaconnector/controller/HookController.java
@@ -59,6 +59,7 @@ public class HookController {
     @PostMapping("/requestsListener")
     public ResponseEntity<?> receiveRequestHook(@RequestBody RequestHook hookContent, @RequestHeader("X-Exl-Signature") String signature) {
         log.info(String.format("revceived hook of type %s", hookContent.getAction()));
+        log.info(hookContent.getUserRequest().toString());
         this.hookService.processRequestHook(hookContent);
         return ResponseEntity.ok().build();
     }
