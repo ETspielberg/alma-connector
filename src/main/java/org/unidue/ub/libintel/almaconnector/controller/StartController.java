5b8a5de8673eb9d7b64112a21e4bdf32c7ca3a54
Eike Spielberg <Eike.Spielberg@uni-due.de>
2021-05-28 20:36:56 +0200

    adding error display page

--- src/main/java/org/unidue/ub/libintel/almaconnector/controller/StartController.java
+++ src/main/java/org/unidue/ub/libintel/almaconnector/controller/StartController.java
@@ -1,7 +1,12 @@
 package org.unidue.ub.libintel.almaconnector.controller;
 
+import org.apache.commons.io.IOExceptionWithCause;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.net.UnknownHostException;
 
 /**
  * the simple page controllers
@@ -16,5 +21,19 @@ public class StartController {
     @GetMapping("/start")
     public String getStartPage() {
         return "start";
+    }
+
+    /**
+     * Exception handling if one of the following exceptions is thrown: MissingShibbolethDataException, MissingHisDataException, AlmaConnectionException
+     *
+     * @param ex Exception to be handled
+     * @return the error page bound to the error message.
+     */
+    @ExceptionHandler({UnknownHostException.class, IOExceptionWithCause.class})
+    public ModelAndView handleException(Exception ex) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("error");
+        modelAndView.addObject("message", ex.getMessage());
+        return modelAndView;
     }
 }
