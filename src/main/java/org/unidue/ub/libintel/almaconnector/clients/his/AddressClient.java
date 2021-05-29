f04b843fb573fd544b624e2eb986856313a18bdb
Eike Spielberg <Eike.Spielberg@uni-due.de>
2021-05-29 14:02:57 +0200

    undo

--- src/main/java/org/unidue/ub/libintel/almaconnector/clients/his/AddressClient.java
+++ src/main/java/org/unidue/ub/libintel/almaconnector/clients/his/AddressClient.java
@@ -2,15 +2,16 @@ package org.unidue.ub.libintel.almaconnector.clients.his;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.unidue.ub.libintel.almaconnector.model.his.Address;
 
 @FeignClient(name = "his", url = "https://register.ub.uni-due.de/api", configuration = HisConfiguration.class)
 @Service
 public interface AddressClient {
 
-    @RequestMapping(method= RequestMethod.GET, value="/address")
-    Address getAddressForZimKennung(@RequestParam String zimkennung);
+    @RequestMapping(method= RequestMethod.GET, value="/address/{zimkennung}")
+    Address getAddressForZimKennung(@PathVariable String zimkennung);
+
 }
