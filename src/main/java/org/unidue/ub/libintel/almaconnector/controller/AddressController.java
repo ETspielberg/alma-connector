315274463c895a1916ed6f557ba598ffdda6e746
Eike Spielberg <Eike.Spielberg@uni-due.de>
2021-06-08 08:31:06 +0200

    returning null object if no addresses can be found

--- src/main/java/org/unidue/ub/libintel/almaconnector/controller/AddressController.java
+++ src/main/java/org/unidue/ub/libintel/almaconnector/controller/AddressController.java
@@ -1,11 +1,13 @@
 package org.unidue.ub.libintel.almaconnector.controller;
 
+import feign.FeignException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.unidue.ub.libintel.almaconnector.clients.his.AddressClient;
+import org.unidue.ub.libintel.almaconnector.model.his.Address;
 
 @Controller
 @RequestMapping("/his")
@@ -24,7 +26,13 @@ public class AddressController {
 
     @GetMapping("/address")
     public String getAddressPage(@RequestParam String zimkennung, Model model) {
-        model.addAttribute("address", addressClient.getAddressForZimKennung(zimkennung));
+        Address address;
+        try {
+            address = addressClient.getAddressForZimKennung(zimkennung);
+        } catch (FeignException fe) {
+            address = null;
+        }
+        model.addAttribute("address", address);
         return "his/address";
     }
 }
