a0ecaf90ce72f2a0a1b63fae28f3af68802c9621
Eike Spielberg <Eike.Spielberg@uni-due.de>
2021-07-02 13:12:57 +0200

    setting user group to description

--- src/main/java/org/unidue/ub/libintel/almaconnector/controller/ServiceController.java
+++ src/main/java/org/unidue/ub/libintel/almaconnector/controller/ServiceController.java
@@ -2,10 +2,14 @@ package org.unidue.ub.libintel.almaconnector.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.unidue.ub.alma.shared.user.AlmaUser;
 import org.unidue.ub.libintel.almaconnector.service.alma.AlmaSetService;
+import org.unidue.ub.libintel.almaconnector.service.alma.AlmaUserService;
 
 @Controller
 @RequestMapping("/services")
@@ -13,8 +17,12 @@ public class ServiceController {
 
     private final AlmaSetService almaSetService;
 
-    ServiceController(AlmaSetService almaSetService) {
+    private final AlmaUserService almaUserService;
+
+    ServiceController(AlmaSetService almaSetService,
+                      AlmaUserService almaUserService) {
         this.almaSetService = almaSetService;
+        this.almaUserService = almaUserService;
     }
 
     @PostMapping("/scanOut/{setId}")
@@ -24,5 +32,22 @@ public class ServiceController {
             return ResponseEntity.ok().build();
         else
             return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/bereitstellungen")
+    private String getHolds(String userId, Model model) throws UserIdInvalidException, UserNotFoundException {
+        if ((userId == null) || !userId.trim().matches("[a-zA-Z0-9\\-]+")) {
+            throw new UserIdInvalidException();
+        }
+        AlmaUser almaUser = almaUserService.getUser(userId);
+        if (almaUser == null)
+            throw new UserNotFoundException();
+        return "bereitstellungen";
+    }
+
+    private class UserIdInvalidException extends Throwable {
+    }
+
+    private class UserNotFoundException extends Throwable {
     }
 }
