8de217525f289b40aafede75ffd5745331cab3ec
Eike Spielberg <Eike.Spielberg@uni-due.de>
2022-01-19 18:04:09 +0100

    refactoring rest repository into repository

--- src/main/java/org/unidue/ub/libintel/almaconnector/repository/jpa/JobIdWithDescriptionRepository.java
+++ src/main/java/org/unidue/ub/libintel/almaconnector/repository/jpa/JobIdWithDescriptionRepository.java
@@ -1,12 +1,12 @@
 package org.unidue.ub.libintel.almaconnector.repository.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 import org.unidue.ub.libintel.almaconnector.model.jobs.JobIdWithDescription;
 
 import java.util.List;
 
-@RepositoryRestResource(collectionResourceRel = "jobsIdWithDescription", path = "jobsIdWithDescription")
+@Repository
 public interface JobIdWithDescriptionRepository extends JpaRepository<JobIdWithDescription, String> {
 
     List<JobIdWithDescription> findAllByCategoryContainingOrDescriptionContainingOrNameContaining(String category, String description,String name);
