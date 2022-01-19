7972306547918e9e683d9eacd37f0dd187b5c56b
Eike Spielberg <Eike.Spielberg@uni-due.de>
2022-01-19 17:52:47 +0100

    removing blocked id service, classes and repository

--- src/main/java/org/unidue/ub/libintel/almaconnector/repository/BlockedIdRepository.java
+++
@@ -1,7 +0,0 @@
-package org.unidue.ub.libintel.almaconnector.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.unidue.ub.libintel.almaconnector.model.BlockedId;
-
-public interface BlockedIdRepository extends CrudRepository<BlockedId, String> {
-}
