4c737b05ccb1fc407d7c012c60913d38ad6c6762
Eike Spielberg <Eike.Spielberg@uni-due.de>
2022-01-19 18:03:49 +0100

    adding mail template for error mails

--- src/main/java/org/unidue/ub/libintel/almaconnector/service/MailSenderService.java
+++ src/main/java/org/unidue/ub/libintel/almaconnector/service/MailSenderService.java
@@ -10,6 +10,9 @@ import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 import org.unidue.ub.alma.shared.bibs.Item;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * handles the sending of mails
  */
@@ -22,20 +25,26 @@ public class MailSenderService {
     @Value("${libintel.regalfinder.email.from:no-reply@uni-due.de}")
     private String regalfinderMailFrom;
 
-    // the email address to appear as "from"
+    // the list of recipients for the regalfinder mail
     @Value("${libintel.regalfinder.email.to}")
     private String[] regalfinderMailTo;
+
+    // the list of recipients for the regalfinder mail
+    @Value("${libintel.admins.mail}")
+    private String[] libintelAdmins;
 
     private final JavaMailSender emailSender;
 
     private final TemplateEngine templateEngine;
 
+    private final static SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
 
     /**
      * constructor based autowiring of email sender and the appropriate mail creation services.
      *
-     * @param emailSender                 the mail sender service as instantiated from the config properties
-     * @param templateEngine              the thymeleaf templating engine
+     * @param emailSender    the mail sender service as instantiated from the config properties
+     * @param templateEngine the thymeleaf templating engine
      */
     MailSenderService(JavaMailSender emailSender, TemplateEngine templateEngine) {
         this.emailSender = emailSender;
@@ -43,7 +52,7 @@ public class MailSenderService {
     }
 
     /**
-     * sends the mail, if an ebook is requested
+     * sends the mail, if an item is not found in the regalfinder
      *
      * @param item the item which is not found by the regalfinder
      */
@@ -58,6 +67,22 @@ public class MailSenderService {
         emailSender.send(messagePreparator);
     }
 
+    /**
+     * sends the mail, if an item is not found in the regalfinder
+     *
+     * @param exception the exception which occured
+     */
+    public void sendAlertMail(Exception exception) {
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom(regalfinderMailFrom);
+            messageHelper.setTo(libintelAdmins);
+            messageHelper.setSubject("Ein Fehler ist aufgetreten!");
+            messageHelper.setText(buildExceptionMail(exception), true);
+        };
+        emailSender.send(messagePreparator);
+    }
+
     private String buildRegalfinderMail(Item item) {
         Context context = new Context();
         context.setVariable("collection", item.getItemData().getLocation().getValue());
@@ -66,5 +91,16 @@ public class MailSenderService {
         context.setVariable("title", item.getBibData().getTitle());
         context.setVariable("author", item.getBibData().getAuthor());
         return templateEngine.process("regalfinderNotificationMailTemplate", context);
-        }
+    }
+
+    private String buildExceptionMail(Exception exception) {
+        Context context = new Context();
+        context.setVariable("timestamp", dateformat.format(new Date()));
+        context.setVariable("message", exception.getMessage());
+        context.setVariable("stacktrace", exception.getStackTrace());
+        context.setVariable("cause", exception.getCause());
+        context.setVariable("class", exception.getClass());
+        context.setVariable("localized", exception.getLocalizedMessage());
+        return templateEngine.process("errorMail", context);
+    }
 }
