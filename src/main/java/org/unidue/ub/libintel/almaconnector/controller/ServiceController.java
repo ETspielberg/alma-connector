e2e8bdad76ef170eea8c0d9078a053537dc51d99
Eike Spielberg <Eike.Spielberg@uni-due.de>
2022-01-19 17:53:47 +0100

    refactoring services endpoint to restful endpoints

--- src/main/java/org/unidue/ub/libintel/almaconnector/controller/ServiceController.java
+++ src/main/java/org/unidue/ub/libintel/almaconnector/controller/ServiceController.java
@@ -2,14 +2,11 @@ package org.unidue.ub.libintel.almaconnector.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.unidue.ub.alma.shared.user.AlmaUser;
 import org.unidue.ub.libintel.almaconnector.service.RegalfinderService;
 import org.unidue.ub.libintel.almaconnector.service.SaveDataService;
 import org.unidue.ub.libintel.almaconnector.service.ScheduledService;
-import org.unidue.ub.libintel.almaconnector.service.alma.AlmaUserService;
 
 import java.io.IOException;
 
@@ -17,29 +14,19 @@ import java.io.IOException;
 @RequestMapping("/services")
 public class ServiceController {
 
-    private final AlmaUserService almaUserService;
-
     private final ScheduledService scheduledService;
 
     private final RegalfinderService regalfinderService;
 
     private final SaveDataService saveDataService;
 
-    ServiceController(AlmaUserService almaUserService,
-                      ScheduledService scheduledService,
+    ServiceController(ScheduledService scheduledService,
                       RegalfinderService regalfinderService,
                       SaveDataService saveDataService) {
-        this.almaUserService = almaUserService;
         this.scheduledService = scheduledService;
         this.regalfinderService = regalfinderService;
         this.saveDataService = saveDataService;
 
-    }
-
-    @GetMapping("/scan")
-    private String showScanOutForm(Model model) {
-        model.addAttribute("setId", "");
-        return "services/scan";
     }
 
     @GetMapping("/collectRequests")
@@ -54,41 +41,21 @@ public class ServiceController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/bereitstellungen")
-    private String getHolds(String userId, Model model) throws UserIdInvalidException, UserNotFoundException {
-        if ((userId == null) || !userId.trim().matches("[a-zA-Z0-9\\-]+")) {
-            throw new UserIdInvalidException("User ID is not valid");
-        }
-        AlmaUser almaUser = almaUserService.getUser(userId);
-        if (almaUser == null)
-            throw new UserNotFoundException("Alma User not found");
-        return "bereitstellungen";
-    }
-
     @GetMapping("/usersToEnd")
-    private String updateUsersToEnd(){
+    private ResponseEntity<?> updateUsersToEnd(){
         this.scheduledService.runEndingUserNotificationJob();
-        return "services/finished";
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/saveDailyUserFineFees")
-    public String saveDailyUserFineFees() {
+    public ResponseEntity<?> saveDailyUserFineFees() throws IOException {
         this.saveDataService.saveDailyUserFineFees();
-        return "services/finished";
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/saveInitialUserFineFees")
-    public String saveInitialUserFineFees() {
+    public ResponseEntity<?> saveInitialUserFineFees() {
         this.saveDataService.saveInitialUserFineFees();
-        return "services/finished";
-    }
-
-    private static class UserIdInvalidException extends RuntimeException {
-        UserIdInvalidException(String message) { super(message); }
-
-    }
-
-    private static class UserNotFoundException extends RuntimeException {
-        UserNotFoundException(String message) { super(message); }
+        return ResponseEntity.ok().build();
     }
 }
